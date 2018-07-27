bade8c70ca9a0bfdb892fa7684c500a1524d8279
Karim Aouadi <kdolphinus@gmail.com>
2018-07-27 19:25:52 +0200

    Updated store

--- src/components/ProductListOne.vue
+++ src/components/ProductListOne.vue
@@ -7,6 +7,7 @@
             <span class="price">{{product.price}}</span>
         </li>
     </ul>
+    <button v-on:click="reducePrice">Reduce Price</button>
 </div>
     
 </template>
@@ -24,6 +25,13 @@ computed:{
        
     } 
 
+
+ },
+ methods:{
+
+     reducePrice: function(){
+         this.$store.commit('reducePrice')
+     }
 
  }
 }
--- src/store/index.js
+++ src/store/index.js
@@ -27,6 +27,14 @@ export const store = new Vuex.Store({
             return saleProducts;
         } 
 
+    },
+    mutations:{
+        reducePrice: function(state){
+            state.products.forEach(product => {
+                product.price -= 1
+            });
+
+        }
     }
 
 })
