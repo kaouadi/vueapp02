a0cc04bc478522e4f504bb1f8d95191262c31a89
Karim Aouadi <kdolphinus@gmail.com>
2018-07-27 15:12:34 +0200

    first repo

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,30 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <product-list-one v-bind:products="products"></product-list-one>
+    <product-list-two v-bind:products="products">></product-list-two>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ProductListOne from '@/components/ProductListOne.vue'
+import ProductListTwo from '@/components/ProductListTwo.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    'product-list-one': ProductListOne,
+    'product-list-two':ProductListTwo
+  },
+  data(){
+    return {
+      products: [
+        {name: 'Banan Skin', price: 20},
+        {name: 'Banan JK', price: 40},
+        {name: 'Banan LM', price: 60},
+
+      ]
+    }
   }
 }
 </script>
@@ -24,5 +37,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
 }
 </style>
---
+++ src/components/ProductListTwo.vue
@@ -0,0 +1,28 @@
+<template>
+<div class="">
+    <h2>Product List Two</h2><ul>
+        <li v-for="product in products">
+            <span class="name">{{product.name}}</span>
+            <span class="price">{{product.price}}</span>
+        </li>
+    </ul>
+</div>
+    
+</template>
+
+<script>
+export default {
+    props:['products'],
+    data(){
+        return {
+
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
+
